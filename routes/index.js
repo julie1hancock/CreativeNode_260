ac0c1b2d0354b1992cd74b0dbca75a1ac9e7d6d1
EC2 Default User <ec2-user@ip-172-31-14-158.us-east-2.compute.internal>
2018-11-01 21:15:22 +0000

    done fool

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,31 @@ router.get('/getfact',function(req,res,next) {
     });
 });
 
+router.get('/getquote',function(req,res,next) {
+    var myNum = req.query.q;
+    fs.readFile(__dirname + '/quotes.txt',function(err,data) {
+        if(err) throw err;
+        var facts = data.toString().split("\n");
+        var q = facts[myNum];
+        var s = q.toString().split("-");
+        var jsonresult = [];
+        jsonresult.push({fact:s[0], cite:s[1]});
+        console.log(jsonresult);
+        res.status(200).json(jsonresult);
+    });
+});
+
+router.get('/getimg',function(req,res,next) {
+    var myNum = req.query.q;
+    fs.readFile(__dirname + '/pics.txt',function(err,data) {
+        if(err) throw err;
+        var facts = data.toString().split("\n");
+      
+        var jsonresult = [];
+        jsonresult.push({fact:facts[myNum]});
+        console.log(jsonresult);
+        res.status(200).json(jsonresult);
+    });
+});
+
 module.exports = router;
